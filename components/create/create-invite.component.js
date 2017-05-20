5f3a2009f7824b7acf7a17c5e6683346f1ea287b
Admin <admin@Admins-Mac.local>
2017-05-20 08:05:32 +0300

    Create event flow done.

--- components/create/create-invite.component.js
+++ components/create/create-invite.component.js
@@ -9,42 +9,89 @@ import {
 import { Icon } from 'react-native-elements';
 import { NavigationActions } from 'react-navigation';
 
-import friends from '../../samples/friends';
+import http from '../../services/http.service';
 
 export default class CreateNewEventComponent extends Component {
   constructor(props) {
     super(props);
 
     this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.contributors = {};
+
     this.state = {
-      data: friends,
-      dataSource: this.ds.cloneWithRows(friends),
-      invitees: []
+      data: [],
+      dataSource: this.ds.cloneWithRows([]),
     }
   }
 
+  componentDidMount() {
+    http.get('/api/friends')
+      .then(friends => {
+        this.setState({
+          data: friends,
+          dataSource: this.ds.cloneWithRows(friends)
+        });
+      })
+      .catch(error => { });
+  }
+
   complete = () => {
-    console.log('complete')
-    this.props.navigation.dispatch(NavigationActions.reset({
-      actions: [
-        NavigationActions.navigate({
-          routeName: 'CreateCompleteComponent',
-          params: {
-            event: this.props.navigation.state.params.event,
-            invitees: this.state.invitees
-          }
-        })
-      ],
-      index: 0
-    }))
+    const event = {
+      ...this.props.navigation.state.params.event,
+      contributors: this.contributors
+    };
+
+    http.post('/api/events', JSON.stringify(event))
+      .then(() => {
+        this.props.navigation.dispatch(NavigationActions.reset({
+          actions: [
+            NavigationActions.navigate({
+              params: {
+                event: event,
+                isNew: true
+              },
+              routeName: 'CreateCompleteComponent'
+            })
+          ],
+          index: 0
+        }));
+      })
+      .catch(error => { });
+  }
+
+  invite = (rowData, rowID) => {
+    // Add contributor if not already invited:
+    if (!rowData.invited) {
+      const _data = this.state.data.slice();
+      _data[rowID].invited = true;
+      this.contributors[rowID] = true;
+
+      this.setState({
+        data: _data,
+        dataSource: this.ds.cloneWithRows(_data)
+      });
+    }
+    // Else remove contributor: 
+    else {
+      const _data = this.state.data.slice();
+      _data[rowID].invited = false;
+      delete this.contributors[rowID];
+
+      this.setState({
+        data: _data,
+        dataSource: this.ds.cloneWithRows(_data)
+      });
+    }
   }
 
   render() {
+    const { event } = this.props.navigation.state.params;
+
     return (
       <View style={{ flex: 1 }}>
 
         {/* Header */}
-        <View style={{ alignItems: 'center', flex: 1, flexDirection: 'row', marginTop: 20 }}>
+        <View style={styles.header}>
           <View style={{ flex: 1 }}>
             <Icon
               name='angle-left'
@@ -55,30 +102,43 @@ export default class CreateNewEventComponent extends Component {
               underlayColor='transparent' />
           </View>
           <View style={{ flex: 10 }}>
-            <Text style={{ fontSize: 20, textAlign: 'center' }}>Add to ...</Text>
+            <Text style={styles.headerText}>Add to {event.event}</Text>
           </View>
           <View style={{ flex: 1 }}>
           </View>
         </View>
 
         {/* Body */}
-        <View style={{ flex: 10, padding: 20 }}>
-          <ListView
-            dataSource={this.state.dataSource}
-            removeClippedSubviews={false}
-            renderRow={(rowData, sectionID, rowID) => (
-              <View style={{ alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', padding: 5 }}>
-                <Text style={{ fontSize: 16 }}>{rowData.username}</Text>
-                <TouchableHighlight
-                  onPress={() => { }}
-                  underlayColor='transparent'>
-                  <View style={{ alignItems: 'center', borderWidth: 0.5, borderRadius: 10, flexDirection: 'row', paddingHorizontal: 10 }}>
-                    <Text>Invite</Text>
-                    <Icon name='add' />
+        <View style={styles.body}>
+          {
+            this.state.data.length > 0 ?
+              <ListView
+                dataSource={this.state.dataSource}
+                removeClippedSubviews={false}
+                renderRow={(rowData, sectionID, rowID) => (
+                  <View style={styles.row}>
+                    <Text style={{ fontSize: 16 }}>{rowData.username}</Text>
+                    <TouchableHighlight
+                      onPress={() => this.invite(rowData, rowID)}
+                      underlayColor='transparent'>
+
+                      {
+                        rowData.invited ?
+                          <View style={styles.invited}>
+                            <Text>Invited</Text>
+                            <Icon name='add' />
+                          </View> :
+                          <View style={styles.invite}>
+                            <Text>Invite</Text>
+                            <Icon name='add' />
+                          </View>
+                      }
+
+                    </TouchableHighlight>
                   </View>
-                </TouchableHighlight>
-              </View>
-            )} />
+                )} /> :
+              <Text style={styles.grayText}>No friends were found</Text>
+          }
         </View>
 
         {/* Footer */}
@@ -101,10 +161,48 @@ export default class CreateNewEventComponent extends Component {
 }
 
 const styles = StyleSheet.create({
+  body: {
+    flex: 10,
+    padding: 20
+  },
   footer: {
     alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'flex-end',
     padding: 10
+  },
+  grayText: {
+    color: 'gray',
+    textAlign: 'center'
+  },
+  header: {
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: 20
+  },
+  headerText: {
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  invite: {
+    alignItems: 'center',
+    borderWidth: 0.5,
+    borderRadius: 10,
+    flexDirection: 'row',
+    paddingHorizontal: 10
+  },
+  invited: {
+    alignItems: 'center',
+    backgroundColor: '#f74434',
+    borderRadius: 10,
+    flexDirection: 'row',
+    paddingHorizontal: 10
+  },
+  row: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 5
   },
 });
--- components/create/create-new-event.component.js
+++ components/create/create-new-event.component.js
@@ -1,6 +1,7 @@
 const moment = require('moment');
 import React, { Component } from 'react';
 import {
+  Dimensions,
   Image,
   Keyboard,
   SegmentedControlIOS,
@@ -30,18 +31,17 @@ export default class CreateNewEventComponent extends Component {
     };
 
     this.state = {
-      cover: 'images/Background.png',
+      cover: '',
       isVisible: false,
     };
   }
 
   openPicker = () => {
     ImagePicker.openPicker({
-      width: 300,
+      cropping: true,
       height: 120,
-      cropping: true
+      width: Dimensions.get('window').width,
     }).then(image => {
-      console.log(image)
       this.event.cover = image.path;
       this.setState({ cover: image.path });
     }).catch(() => { });
@@ -50,121 +50,121 @@ export default class CreateNewEventComponent extends Component {
   render() {
     return (
       <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-        {/*<KeyboardAwareScrollView>*/}
-        <View style={{ flex: 1 }}>
-          <StatusBar hidden={false} />
+        <KeyboardAwareScrollView>
+          <View style={{ flex: 1 }}>
+            <StatusBar hidden={false} />
 
-          {/* Header */}
-          <View style={{ alignItems: 'center', flex: 1, flexDirection: 'row', marginTop: 20 }}>
-            <View style={{ flex: 1 }}>
+            {/* Header */}
+            <View style={styles.header}>
+              <View style={{ flex: 1 }}>
+                <Icon
+                  name='angle-left'
+                  onPress={() => this.props.navigation.goBack()}
+                  size={40}
+                  style={{ marginLeft: 10 }}
+                  type='font-awesome'
+                  underlayColor='transparent' />
+              </View>
+              <View style={{ flex: 10 }}>
+                <Text style={styles.headerText}>Create a Catch</Text>
+              </View>
+              <View style={{ flex: 1 }}>
+              </View>
+            </View>
+
+            {/* Body */}
+            <View style={{ flex: 10 }}>
+
+              {/* Cover photo */}
+              <TouchableHighlight onPress={this.openPicker} underlayColor='transparent'>
+                <Image
+                  source={this.state.cover ? { uri: this.state.cover } :
+                    require('../../images/Background.png')}
+                  style={styles.cover}>
+                  <View style={styles.coverView}>
+                    <Text style={{ color: 'white' }}>Add a cover photo</Text>
+                    <Icon color='white' name='add-circle-outline' />
+                  </View>
+                </Image>
+              </TouchableHighlight>
+
+              {/* Form */}
+              <View style={{ padding: 20 }}>
+                {/* Event */}
+                <Text style={{ fontSize: 16 }}>Event</Text>
+                <TextInput
+                  autoCapitalize='words'
+                  autoCorrect={true}
+                  onChangeText={event => this.event.event = event}
+                  style={styles.textInput}
+                />
+
+                {/* Open on */}
+                <TouchableHighlight
+                  onPress={() => this.setState({ isVisible: true })}
+                  underlayColor='transparent'>
+                  <View>
+                    <Text style={{ fontSize: 16 }}>Open on</Text>
+                    <TextInput
+                      editable={false}
+                      style={styles.textInput}
+                      value={moment(this.event.date).format('MMMM D YYYY, h:mm a')} />
+                  </View>
+                </TouchableHighlight>
+
+                {/* Open on */}
+                <DateTimePicker
+                  isVisible={this.state.isVisible}
+                  maximumDate={new Date(new Date().setFullYear(new Date().getFullYear() + 1))}
+                  minimumDate={new Date()}
+                  mode='datetime'
+                  onCancel={() => this.setState({ isVisible: false })}
+                  onConfirm={date => {
+                    this.event.date = new Date(date).getTime();
+                    this.setState({ isVisible: false });
+                  }} />
+
+                {/* Who */}
+                <Text style={{ fontSize: 16 }}>Who will view this?</Text>
+                <SegmentedControlIOS
+                  onChange={event => this.event.audience = event.nativeEvent.selectedSegmentIndex}
+                  selectedIndex={this.event.audience}
+                  style={{ marginBottom: 20 }}
+                  tintColor='#f74434'
+                  values={['Public', 'Private']} />
+
+
+                {/* Description */}
+                <Text style={{ fontSize: 16 }}>Description</Text>
+                <TextInput
+                  autoCapitalize='sentences'
+                  autoCorrect={true}
+                  multiline={true}
+                  onChangeText={description => this.event.description = description}
+                  style={styles.description} />
+              </View>
+
+            </View>
+
+            {/*Footer*/}
+            <View style={styles.footer}>
+              <Text
+                onPress={() =>
+                  this.props.navigation.navigate('CreateInviteComponent', { event: this.event })}
+                style={{ fontSize: 20 }}>
+                {'Invite Contributors  '}
+              </Text>
               <Icon
-                name='angle-left'
-                onPress={() => this.props.navigation.goBack()}
+                name='angle-right'
+                onPress={() =>
+                  this.props.navigation.navigate('CreateInviteComponent', { event: this.event })}
                 size={40}
-                style={{ marginLeft: 10 }}
                 type='font-awesome'
                 underlayColor='transparent' />
             </View>
-            <View style={{ flex: 10 }}>
-              <Text style={{ fontSize: 20, textAlign: 'center' }}>Create a Catch</Text>
-            </View>
-            <View style={{ flex: 1 }}>
-            </View>
-          </View>
-
-          {/* Body */}
-          <View style={{ flex: 10 }}>
-
-            {/* Cover photo */}
-            <TouchableHighlight onPress={this.openPicker} underlayColor='transparent'>
-              <Image source={{ uri: this.state.cover }} style={styles.cover}>
-                <View style={styles.coverView}>
-                  <Text style={{ color: 'white' }}>Add a cover photo</Text>
-                  <Icon color='white' name='add-circle-outline' />
-                </View>
-              </Image>
-            </TouchableHighlight>
-
-            {/* Form */}
-            <View style={{ padding: 20 }}>
-              {/* Event */}
-              <Text style={{ fontSize: 16 }}>Event</Text>
-              <TextInput
-                autoCapitalize='words'
-                autoCorrect={true}
-                onChangeText={event => this.event.event = event}
-                style={styles.textInput}
-              />
-
-              {/* Open on */}
-              <TouchableHighlight
-                onPress={() => this.setState({ isVisible: true })}
-                underlayColor='transparent'>
-                <View>
-                  <Text style={{ fontSize: 16 }}>Open on</Text>
-                  <TextInput
-                    editable={false}
-                    style={styles.textInput}
-                    value={moment(this.event.date).format('MMMM D YYYY, h:mm a')} />
-                </View>
-              </TouchableHighlight>
-
-              {/* Open on */}
-              <DateTimePicker
-                isVisible={this.state.isVisible}
-                maximumDate={new Date(new Date().setFullYear(new Date().getFullYear() + 1))}
-                minimumDate={new Date()}
-                mode='datetime'
-                onCancel={() => this.setState({ isVisible: false })}
-                onConfirm={date => {
-                  this.event.date = date;
-                  this.setState({ isVisible: false });
-                }} />
-
-              {/* Who */}
-              <Text style={{ fontSize: 16 }}>Who will view this?</Text>
-              <SegmentedControlIOS
-                onChange={event => this.event.audience = event.nativeEvent.selectedSegmentIndex}
-                selectedIndex={this.event.audience}
-                style={{ marginBottom: 20 }}
-                tintColor='#f74434'
-                values={['Public', 'Private']} />
-
-
-              {/* Description */}
-              <Text style={{ fontSize: 16 }}>Description</Text>
-              <TextInput
-                autoCapitalize='sentences'
-                autoCorrect={true}
-                multiline={true}
-                onChangeText={description => {
-                  this.event.description = description
-                  console.log(this.event)
-                }}
-                style={styles.description} />
-            </View>
 
           </View>
-
-          {/*Footer*/}
-          <View style={styles.invite}>
-            <Text
-              onPress={() =>
-                this.props.navigation.navigate('CreateInviteComponent', { event: this.state })}
-              style={{ fontSize: 20 }}>
-              {'Invite Contributors  '}
-            </Text>
-            <Icon
-              name='angle-right'
-              onPress={() =>
-                this.props.navigation.navigate('CreateInviteComponent', { event: this.state })}
-              size={40}
-              type='font-awesome'
-              underlayColor='transparent' />
-          </View>
-
-        </View>
-        {/*</KeyboardAwareScrollView>*/}
+        </KeyboardAwareScrollView>
       </TouchableWithoutFeedback>
     );
   }
@@ -176,6 +176,7 @@ const styles = StyleSheet.create({
     height: 120,
     justifyContent: 'flex-end',
     marginTop: 20,
+    width: Dimensions.get('window').width
   },
   coverView: {
     alignItems: 'center',
@@ -191,11 +192,21 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     borderWidth: 0.5,
     fontSize: 16,
-    height: 60,
+    height: 100,
     marginBottom: 20,
     padding: 5
   },
-  invite: {
+  header: {
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: 20
+  },
+  headerText: {
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  footer: {
     alignItems: 'center',
     flex: 1,
     flexDirection: 'row',
