2abafb2c279d7261ffcd072f2e85f260417b961f
George Miranda <George Miranda>
2017-05-31 17:58:46 -0700

    fix input boxes for android

--- components/account/account-settings.component.js
+++ components/account/account-settings.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {
   Image,
+  Platform,
   StyleSheet,
   Text,
   TextInput,
@@ -141,3 +142,11 @@ const styles = StyleSheet.create({
     marginTop: 10
   },
 });
+
+if (Platform.OS === 'android'){
+  styles.inputText = {
+    fontSize: 16,
+    textAlign: 'center'
+  };
+  styles.inputView = undefined;
+}
